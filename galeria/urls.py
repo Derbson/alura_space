9f51238642ed0274e7943fd5978424b7444482fe
Derbson <pedrin.olinda@gmail.com>
2023-02-12 15:24:43 -0300

    adição de templates

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, imagem
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
 
 
--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
 
 
 
