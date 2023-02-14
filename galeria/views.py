e364729c62fed32f9b33432dec595de2535fddd6
Derbson <pedrin.olinda@gmail.com>
2023-02-14 17:56:41 -0300

    função de busca na pagina inicial

--- galeria/views.py
+++ galeria/views.py
@@ -8,8 +8,16 @@ def index(request):
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
+
+def buscar(request):
+    fotografia = Fotografia.objects.order_by("-id").filter(publicado=True)
+
+    nome_a_buscar = request.GET['buscar']
+    if nome_a_buscar:
+        fotografia = fotografia.filter(nome__icontains=nome_a_buscar)
+    
+    return render(request, "galeria/buscar.html", {"cards": fotografia})
 
 
