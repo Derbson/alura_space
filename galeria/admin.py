c98fda43cac3af915ecee044dfffb11d66ad3fa8
Derbson <pedrin.olinda@gmail.com>
2023-02-14 12:25:42 -0300

    crud e configurações no galeria/admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Fotografia
 
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_per_page = 5
 
-
-
-admin.site.register(Fotografia)
--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from galeria.models import Fotografia
-
+from .models import Fotografia
 
 def index(request):
     fotografia = Fotografia.objects.all()
     
-    return render(request, 'galeria/index.html', {'cards': fotografia})
+    return render(request, 'galeria/index.html', {"cards": fotografia})
 
 def imagem(request, foto_id):
-    
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     
-    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 
 
